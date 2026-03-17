ce5eaf4810e2d8cafc8ef96d0038eb8be2022648
Meejuru Eswar <meejurueswar9@gmail.com>
2026-03-17 22:30:54 +0530

    added random  background cover image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,33 @@
+import { useRef, useState } from "react";
 import Header from "../components/Header";
 
 function Home() {
+  const id = useRef(25);
+  const [bgImage, setBgImage] = useState("https://picsum.photos/id/25/1920/1080");
   const handleClick = () => {
-    // TODO: Add functionality if needed
+    id.current += 1;
+    setBgImage(`https://picsum.photos/id/${id.current}/1920/1080`);
   };
 
   return (
     <>
       <Header />
+        <div 
+          style={{
+          backgroundImage: bgImage ? `url(${bgImage})` : "none",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          position: "fixed",
+          top: "60px",
+          left: 0,
+          width: "100%",
+          height: "calc(100% - 60px)",
+          zIndex: -1
+        }}
+        >
+      </div>
       <div className="container">
-        <p>
-          Explore and interact with APIs effortlessly. Click the button below to fetch and view user data from our sample API.
-        </p>
         <button onClick={handleClick}>
           Next Background
         </button>
